973f5d14fa04f5f9493f08991fa1452d4f018b24
Aditya Padmakumar <aditya2506@ucla.edu>
2016-02-10 23:38:01 -0800

    Part3 + DBaccess

---
+++ Part3/project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -0,0 +1,116 @@
+package edu.ucla.cs.cs144;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.File;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class Indexer {
+    
+    /** Creates a new instance of Indexer */
+    public Indexer() {
+    }
+    
+    private IndexWriter indexWriter = null;
+    
+    public IndexWriter getIndexWriter(boolean create) throws IOException {
+        if (indexWriter == null) {
+            Directory indexDir = FSDirectory.open(new File("index-directory"));
+            IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
+            indexWriter = new IndexWriter(indexDir, config);
+        }
+        return indexWriter;
+   }    
+   
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            indexWriter.close();
+        }
+   }
+    public void rebuildIndexes() throws IOException {
+
+        Connection conn = null;
+        getIndexWriter(true);
+          // create a connection to the database to retrieve Items from MySQL
+	try {
+	    conn = DbManager.getConnection(true);
+        Statement s = conn.createStatement() ;
+        //s.executeUpdate("CREATE TABLE Sellers(bar VARCHAR(40), beer VARCHAR(40), price REAL)" ) ;
+        ResultSet rs = s.executeQuery("SELECT * FROM Users WHERE Rating > 10000") ;
+        String userid, rating ;
+        while( rs.next() ){
+                 userid = rs.getString("UserId");
+                 rating = rs.getString("Rating");
+                 System.out.println(userid + " has a Rating of: " + rating);
+            }
+        rs.close();
+        s.close();
+        conn.close();
+        //System.out.println("Hellooo");
+
+	} catch (SQLException ex){
+            System.out.println("SQLException caught");
+            System.out.println("---");
+            while ( ex != null ){
+                System.out.println("Message   : " + ex.getMessage());
+                System.out.println("SQLState  : " + ex.getSQLState());
+                System.out.println("ErrorCode : " + ex.getErrorCode());
+                System.out.println("---");
+                ex = ex.getNextException();
+            }
+       }
+
+
+	/*
+	 * Add your code here to retrieve Items using the connection
+	 * and add corresponding entries to your Lucene inverted indexes.
+         *
+         * You will have to use JDBC API to retrieve MySQL data from Java.
+         * Read our tutorial on JDBC if you do not know how to use JDBC.
+         *
+         * You will also have to use Lucene IndexWriter and Document
+         * classes to create an index and populate it with Items data.
+         * Read our tutorial on Lucene as well if you don't know how.
+         *
+         * As part of this development, you may want to add 
+         * new methods and create additional Java classes. 
+         * If you create new classes, make sure that
+         * the classes become part of "edu.ucla.cs.cs144" package
+         * and place your class source files at src/edu/ucla/cs/cs144/.
+	 * 
+	 */
+
+
+        // close the database connection
+      
+        closeIndexWriter();      
+
+    }    
+
+    public static void main(String args[]) {
+
+    try {
+        Indexer idx = new Indexer();
+        idx.rebuildIndexes();
+    } catch (Exception e) {
+        System.out.println("Exception caught.\n");
+    }
+    }   
+}
